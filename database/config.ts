66c45632e8036efceb9af919919a72eee41b0cf5
Sthefano Fedora <sthefanopoli@gmail.com>
2024-07-23 23:33:07 -0300

    Adding the database directory

---
+++ database/config.ts
@@ -0,0 +1,12 @@
+// const mongoose=require('mongoose')
+// const dbConnection=async()=>{
+// try{
+//     connect=await mongoose.connect(process.env.CONNMONGODBATLAS)
+//     console.log('Conectado a la base de datos');
+// }catch(error){
+//     console.log(error);
+//     throw new Error("Error a la hora de conectar con la base de datos") //forma de lanzar errores
+// }
+// }    
+// module.exports={dbConnection}
+// ejemplo de conexion con mongo db atlas
